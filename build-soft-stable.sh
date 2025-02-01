6777d0e90b3ecb37188d5fea0d15163e5a7a4897
Dušan Poizl <nou.spiro@gmail.com>
2025-02-01 12:42:59 +0100

    Update INDI 2.1.2, KStars 3.7.5

--- build-soft-stable.sh
+++ build-soft-stable.sh
@@ -3,11 +3,11 @@
 export CFLAGS="-march=native -w -Wno-psabi -D_FILE_OFFSET_BITS=64"
 export CXXFLAGS="-march=native -w -Wno-psabi -D_FILE_OFFSET_BITS=64"
 
-LIBXISF_COMMIT="v0.2.12"
-INDI_COMMIT="v2.1.1"
-INDI_3RD_COMMIT="v2.1.1"
+LIBXISF_COMMIT="v0.2.13"
+INDI_COMMIT="v2.1.2"
+INDI_3RD_COMMIT="v2.1.2"
 STELLAR_COMMIT="157092d6f843fb987818bd61f0b14b440eca3146"
-KSTARS_COMMIT="origin/stable-3.7.4"
+KSTARS_COMMIT="origin/stable-3.7.5"
 
 # you can set custom BUILD_DIR
 BUILD_DIR=${BUILD_DIR:-$HOME}
