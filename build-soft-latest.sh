511b1433c8c2d37db7b522aec7b352ed3a9a0a38
Patrick Molenaar <patrick.molenaar@asmpt.com>
2025-04-28 11:44:18 +0200

    Updated scripts to latest version from nouspiro. Added logging to track build and build times. Updated PHD2 build to latest release. Added log message when PHD2 install failed. Optimized dependencies, remove duplicates from PHD2 dependency install, moved opencv to main depencencies, as KStars needs this depencency for its latest release

--- build-soft-latest.sh
+++ build-soft-latest.sh
@@ -9,6 +9,7 @@ BUILD_DIR=${BUILD_DIR:-$HOME}
 ROOTDIR="$BUILD_DIR/astro-soft"
 
 JOBS=$(grep -c ^processor /proc/cpuinfo)
+
 # 64 bit systems need more memory for compilation
 if [ $(getconf LONG_BIT) -eq 64 ] && [ $(grep MemTotal < /proc/meminfo | cut -f 2 -d ':' | sed s/kB//) -lt 5000000 ]
 then
@@ -19,6 +20,21 @@ fi
 [ ! -d "$ROOTDIR" ] && mkdir -p "$ROOTDIR"
 cd "$ROOTDIR"
 
+echo "Build dir = $BUILD_DIR"
+echo "Root dir  = $ROOTDIR"
+echo "Jobs      = $JOBS"
+
+echo "Start build at:        `date`" >> $BUILD_DIR/build_log.txt
+
+# delete all previously installed files
+[ -f build-libXISF/install_manifest.txt ] && echo "Deleting libXISF"; cat build-libXISF/install_manifest.txt | sudo xargs rm -f
+[ -f build-indi/install_manifest.txt ] && echo "Deleting INDI"; cat build-indi/install_manifest.txt | sudo xargs rm -f
+[ -f build-indi-lib/install_manifest.txt ] && cat build-indi-lib/install_manifest.txt | sudo xargs rm -f
+[ -f build-indi-3rdparty/install_manifest.txt ] && echo "Deleting INDI 3rdparty"; cat build-indi-3rdparty/install_manifest.txt | sudo xargs rm -f
+[ -f build-stellarsolver/install_manifest.txt ] && echo "Deleting stellarsolver"; cat build-stellarsolver/install_manifest.txt | sudo xargs rm -f
+[ -f build-kstars/install_manifest.txt ] && echo "Deleting KStars"; cat build-kstars/install_manifest.txt | sudo xargs rm -f
+
+echo "building libXISF:      `date`" >> $BUILD_DIR/build_log.txt
 [ ! -d "libXISF" ] && { git clone https://gitea.nouspiro.space/nou/libXISF.git || { echo "Failed to clone LibXISF"; exit 1; } }
 cd libXISF
 git pull origin
@@ -27,6 +43,7 @@ cd ../build-libXISF
 make -j $JOBS || { echo "LibXISF compilation failed"; exit 1; }
 sudo make install || { echo "LibXISF installation failed"; exit 1; }
 
+echo "building INDI:         `date`" >> $BUILD_DIR/build_log.txt
 cd "$ROOTDIR"
 [ ! -d "indi" ] && { git clone --depth=1 https://github.com/indilib/indi.git || { echo "Failed to clone indi"; exit 1; } }
 cd indi
@@ -36,6 +53,7 @@ cd ../build-indi
 make -j $JOBS || { echo "INDI compilation failed"; exit 1; }
 sudo make install || { echo "INDI installation failed"; exit 1; }
 
+echo "building INDI-LIB      `date`" >> $BUILD_DIR/build_log.txt
 cd "$ROOTDIR"
 [ ! -d "indi-3rdparty" ] && { git clone --depth=1 https://github.com/indilib/indi-3rdparty.git || { echo "Failed to clone indi 3rdparty"; exit 1; } }
 cd indi-3rdparty
@@ -45,11 +63,13 @@ cd ../build-indi-lib
 make -j $JOBS || { echo "INDI lib compilation failed"; exit 1; }
 sudo make install || { echo "INDI lib installation failed"; exit 1; }
 
+echo "building INDI-3RDPARTY `date`" >> $BUILD_DIR/build_log.txt
 [ ! -d ../build-indi-3rdparty ] && { cmake -B ../build-indi-3rdparty ../indi-3rdparty -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release || { echo "INDI lib configuration failed"; exit 1; } }
 cd ../build-indi-3rdparty
 make -j $JOBS || { echo "INDI 3rd-party compilation failed"; exit 1; }
 sudo make install || { echo "INDI lib installation failed"; exit 1; }
 
+echo "building STELLARSOLVER `date`" >> $BUILD_DIR/build_log.txt
 cd "$ROOTDIR"
 [ ! -d "stellarsolver" ] && { git clone --depth=1 https://github.com/rlancaste/stellarsolver.git || { echo "Failed to clone stellarsolver"; exit 1; } }
 cd stellarsolver
@@ -59,6 +79,7 @@ cd ../build-stellarsolver
 make -j $JOBS || { echo "Stellarsolver compilation failed"; exit 1; }
 sudo make install || { echo "Stellarsolver installation failed"; exit 1; }
 
+echo "building KSTARS        `date`" >> $BUILD_DIR/build_log.txt
 cd "$ROOTDIR"
 [ ! -d "kstars" ] && { git clone --depth=1 https://invent.kde.org/education/kstars.git || { echo "Failed to clone KStars"; exit 1; } }
 cd kstars
@@ -72,13 +93,14 @@ sudo ldconfig
 
 [ "$1" != "phd2" ] && exit
 
+echo "building PHD2          `date`" >> $BUILD_DIR/build_log.txt
 cd "$ROOTDIR"
-[ ! -d "phd2" ] && { git clone https://github.com/OpenPHDGuiding/phd2.git || { echo "Failed to clone PHD2"; exit 1; } }
+[ ! -d "phd2" ] && { git clone --depth=1 https://github.com/OpenPHDGuiding/phd2.git || { echo "Failed to clone PHD2"; exit 1; } }
 cd phd2
-git fetch origin
-git switch -d --discard-changes "v2.6.12"
-[ ! -d ../build-phd2 ] && cmake -B ../build-phd2 -DCMAKE_BUILD_TYPE=Release || { echo "PHD2 configuration failed"; exit 1; }
+git pull origin
+[ ! -d ../build-phd2 ] && { cmake -B ../build-phd2 ../phd2 -DCMAKE_BUILD_TYPE=Release || { echo "PHD2 configuration failed"; exit 1; } }
 cd ../build-phd2
 make -j $JOBS || { echo "PHD2 compilation failed"; exit 1; }
-sudo make install
+sudo make install || { echo "PHD2 installation failed"; exit 1; }
 
+echo "Finished build at:     `date`" >> $BUILD_DIR/build_log.txt
