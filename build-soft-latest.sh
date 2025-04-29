cc77a2d19b5ba5836a1e178eb3f2388101ed93fb
Patrick Molenaar <patrick.molenaar@asmpt.com>
2025-04-29 11:36:00 +0200

    Improved logging in scripts. Build indi 3rd-party lib and drivers in properly named build folders

--- build-soft-latest.sh
+++ build-soft-latest.sh
@@ -35,56 +35,61 @@ echo "Start build at:        `date`" >> $BUILD_DIR/build_log.txt
 [ -f build-kstars/install_manifest.txt ] && echo "Deleting KStars"; cat build-kstars/install_manifest.txt | sudo xargs rm -f
 [ -f build-phd2/install_manifest.txt ] && echo "Deleting PHD2"; cat build-phd2/install_manifest.txt | sudo xargs rm -f
 
-echo "building libXISF:      `date`" >> $BUILD_DIR/build_log.txt
+
 [ ! -d "libXISF" ] && { git clone https://gitea.nouspiro.space/nou/libXISF.git || { echo "Failed to clone LibXISF"; exit 1; } }
 cd libXISF
 git pull origin
+echo "Build LibXISF: `date`" >> $BUILD_DIR/build_log.txt
 [ ! -d ../build-libXISF ] && { cmake -B ../build-libXISF ../libXISF -DCMAKE_BUILD_TYPE=Release || { echo "LibXISF configuration failed"; exit 1; } }
 cd ../build-libXISF
 make -j $JOBS || { echo "LibXISF compilation failed"; exit 1; }
 sudo make install || { echo "LibXISF installation failed"; exit 1; }
 
-echo "building INDI:         `date`" >> $BUILD_DIR/build_log.txt
+
 cd "$ROOTDIR"
 [ ! -d "indi" ] && { git clone --depth=1 https://github.com/indilib/indi.git || { echo "Failed to clone indi"; exit 1; } }
 cd indi
 git pull origin
+echo "Build INDI: `date`" >> $BUILD_DIR/build_log.txt
 [ ! -d ../build-indi ] && { cmake -B ../build-indi ../indi -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release || { echo "INDI configuration failed"; exit 1; } }
 cd ../build-indi
 make -j $JOBS || { echo "INDI compilation failed"; exit 1; }
 sudo make install || { echo "INDI installation failed"; exit 1; }
 
-echo "building INDI-LIB      `date`" >> $BUILD_DIR/build_log.txt
+
 cd "$ROOTDIR"
 [ ! -d "indi-3rdparty" ] && { git clone --depth=1 https://github.com/indilib/indi-3rdparty.git || { echo "Failed to clone indi 3rdparty"; exit 1; } }
 cd indi-3rdparty
 git pull origin
-[ ! -d ../build-indi-lib ] && { cmake -B ../build-indi-lib ../indi-3rdparty -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_LIBS=1 -DCMAKE_BUILD_TYPE=Release || { echo "INDI lib configuration failed"; exit 1; } }
-cd ../build-indi-lib
-make -j $JOBS || { echo "INDI lib compilation failed"; exit 1; }
-sudo make install || { echo "INDI lib installation failed"; exit 1; }
+echo "Build INDI 3rdparty-lib `date`" >> $BUILD_DIR/build_log.txt
+[ ! -d ../build-indi-3rdparty-lib ] && { cmake -B ../build-indi-3rdparty-lib ../indi-3rdparty -DCMAKE_INSTALL_PREFIX=/usr -DBUILD_LIBS=1 -DCMAKE_BUILD_TYPE=Release || { echo "INDI 3rdparty-lib configuration failed"; exit 1; } }
+cd ../build-indi-3rdparty-lib
+make -j $JOBS || { echo "INDI 3rdparty-lib compilation failed"; exit 1; }
+sudo make install || { echo "INDI 3rdparty-lib installation failed"; exit 1; }
 
-echo "building INDI-3RDPARTY `date`" >> $BUILD_DIR/build_log.txt
-[ ! -d ../build-indi-3rdparty ] && { cmake -B ../build-indi-3rdparty ../indi-3rdparty -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release || { echo "INDI lib configuration failed"; exit 1; } }
+echo "Build INDI 3rd-party (drivers) `date`" >> $BUILD_DIR/build_log.txt
+[ ! -d ../build-indi-3rdparty ] && { cmake -B ../build-indi-3rdparty ../indi-3rdparty -DCMAKE_INSTALL_PREFIX=/usr -DCMAKE_BUILD_TYPE=Release || { echo "INDI 3rd-party configuration failed"; exit 1; } }
 cd ../build-indi-3rdparty
 make -j $JOBS || { echo "INDI 3rd-party compilation failed"; exit 1; }
-sudo make install || { echo "INDI lib installation failed"; exit 1; }
+sudo make install || { echo "INDI 3rd-party installation failed"; exit 1; }
 
-echo "building STELLARSOLVER `date`" >> $BUILD_DIR/build_log.txt
+
 cd "$ROOTDIR"
 [ ! -d "stellarsolver" ] && { git clone --depth=1 https://github.com/rlancaste/stellarsolver.git || { echo "Failed to clone stellarsolver"; exit 1; } }
 cd stellarsolver
 git pull origin
+echo "Build Stellarsolver `date`" >> $BUILD_DIR/build_log.txt
 [ ! -d ../build-stellarsolver ] && { cmake -B ../build-stellarsolver ../stellarsolver -DCMAKE_BUILD_TYPE=Release || { echo "Stellarsolfer configuration failed"; exit 1; } }
 cd ../build-stellarsolver
 make -j $JOBS || { echo "Stellarsolver compilation failed"; exit 1; }
 sudo make install || { echo "Stellarsolver installation failed"; exit 1; }
 
-echo "building KSTARS        `date`" >> $BUILD_DIR/build_log.txt
+
 cd "$ROOTDIR"
 [ ! -d "kstars" ] && { git clone --depth=1 https://invent.kde.org/education/kstars.git || { echo "Failed to clone KStars"; exit 1; } }
 cd kstars
 git pull origin
+echo "Build KStars `date`" >> $BUILD_DIR/build_log.txt
 [ ! -d ../build-kstars ] && { cmake -B ../build-kstars -DBUILD_TESTING=Off ../kstars -DCMAKE_BUILD_TYPE=Release || { echo "KStars configuration failed"; exit 1; } }
 cd ../build-kstars
 make -j $JOBS || { echo "KStars compilation failed"; exit 1; }
@@ -92,16 +97,17 @@ sudo make install || { echo "KStars installation failed"; exit 1; }
 
 sudo ldconfig
 
-[ "$1" != "phd2" ] && exit
+[ "$1" != "phd2" ] && { echo "PHD2 build skipped. Build finished: `date`" >> $BUILD_DIR/build_log.txt; exit; }
 
-echo "building PHD2          `date`" >> $BUILD_DIR/build_log.txt
+
 cd "$ROOTDIR"
 [ ! -d "phd2" ] && { git clone --depth=1 https://github.com/OpenPHDGuiding/phd2.git || { echo "Failed to clone PHD2"; exit 1; } }
 cd phd2
 git pull origin
+echo "Build PHD2 `date`" >> $BUILD_DIR/build_log.txt
 [ ! -d ../build-phd2 ] && { cmake -B ../build-phd2 ../phd2 -DCMAKE_BUILD_TYPE=Release || { echo "PHD2 configuration failed"; exit 1; } }
 cd ../build-phd2
 make -j $JOBS || { echo "PHD2 compilation failed"; exit 1; }
 sudo make install || { echo "PHD2 installation failed"; exit 1; }
 
-echo "Finished build at:     `date`" >> $BUILD_DIR/build_log.txt
+echo "Build finished: `date`" >> $BUILD_DIR/build_log.txt
